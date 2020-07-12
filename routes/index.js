66607d786ba4e5c07d388703635f12f6b893f213
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-07-11 23:33:50 -0500

    add dotenv, listen on port

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.json({ hello: 'World' })
 })
+
+
+router.get('/geolocate', (req, res, next) => {
+  axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${position.coords.latitude}&lon=${position.coords.longitude}&addressdetails=1`)
+    .then(response => res.json(response.data))
+    .catch(err => next(err)) 
+})
+
 
 module.exports = router
