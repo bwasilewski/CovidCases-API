bf99210dd385ea202fd23cdd91dbd989db544f9a
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-07-11 23:32:27 -0500

    remove generated executable

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+
 
 const indexRouter = require('./routes/index')
-//const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -20,5 +21,6 @@ app.use(cookieParser())
 
 app.use('/', indexRouter)
 //app.use('/users', usersRouter)
+app.listen(process.env.PORT, () => console.log(`Listening on http://localhost:${process.env.PORT}`))
 
 module.exports = app
