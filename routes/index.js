a0424c6db279965af7007f86904065992e8d4535
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-07-19 22:52:21 -0500

    remove covidnow zipcode search and replace it with openstreetmap

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,23 @@ router.get('/', function (req, res, next) {
 router.get('/geolocate', (req, res, next) => {
   const { lat, lng } = req.query
   axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&addressdetails=1`)
-    .then(response => res.json(response.data))
-    .catch(err => next(err)) 
+		.then(response => {
+			console.log(response.data)
+			res.json(response.data)
+		})    
+		.catch(err => next(err)) 
 })
 
 
 router.get('/locatebyzip', (req, res, next) => {
   const { zip } = req.query
-  axios.get(`https://api.covidnow.com/v1/local/geocoding?address=${zip}`)
-    .then(response => res.json(response.data))
-    .catch(err => next(err))
+  axios.get(`https://nominatim.openstreetmap.org/search?q=${zip}&format=json`)
+		.then(response => {
+			let results = []
+			if ( response.data.length > 0 ) results = response.data[0]
+			res.json(results)
+		})    
+		.catch(err => next(err))
 })
 
 
