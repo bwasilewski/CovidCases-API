0f8d77d7df68c5db78dfd14249ef0b301e2e9645
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-21 00:30:03 -0500

    remove unused code and move utils in

--- events/index.js
+++ events/index.js
@@ -2,10 +2,6 @@ const fs = require('fs')
 const path = require('path')
 const csv = require('csv-parser')
 
-const filterByState = (slug, results) => {
-	return results.filter(result => result.Province_State === slug)  
-}
-
 module.exports = {
   getCSV: (filePath) => {
 		return new Promise((resolve, reject) => {
@@ -16,5 +12,11 @@ module.exports = {
 				.on('error', err => reject(err))
 				.on('end', () => resolve(results))
 		})
+	},
+	filterByCountry: (country, dataset) => {
+		return dataset.filter( item => item['Country/Region'] === country )
+	},
+	filterByDate: (date, dataset) => {
+		return dataset.filter( item => item[date] !== undefined )
 	}
 }
