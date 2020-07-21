25c2e252d32f239355fe3dc9a964ec0cf0ecaf88
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-21 02:06:07 -0500

    improve the verbosity of the tests

--- events/jhu/global/confirmed.test.js
+++ events/jhu/global/confirmed.test.js
@@ -1,21 +1,32 @@
-const { GlobalByDate, CountryByDate } = require('./confirmed')
+const { GlobalByDate, getCountry } = require('./confirmed')
 
 
-test('CountryByDate: Results', () => {
-	return CountryByDate('Albania', '7/18/20')
-		.then(response => {
-			expect(response.length).toBe(1) 
-			expect(response[0]['Country/Region']).toBeDefined()
-			expect(response[0]['total']).toBeDefined()
-		})
+describe('getCountry', () => {
+	test('should return an array with a single object', () => {
+		return getCountry('Albania')
+			.then(response => {
+				expect(response.length).toBe(1) 
+				expect(response[0]['Country/Region']).toBeDefined()
+			})
+	})
+
+	test('should get the total for a single day', () => {
+		return getCountry('Albania', '7/18/20')
+			.then(response => {
+				expect(response.length).toBe(1) 
+				expect(response[0]['Country/Region']).toBeDefined()
+				expect(response[0]['total']).toBeDefined()
+			})
+	})
+
+	test('should return an empty array when it cant find a given date', () => {
+		return getCountry('Albania', '7/18/16')
+			.then(response => {
+				expect(response.length).toBe(0)
+			})
+	})
 })
 
-test('CountryByDate: No Results', () => {
-	return CountryByDate('Albania', '7/18/16')
-		.then(response => {
-			expect(response.length).toBe(0)
-		})
-})
 
 test('GlobalByDate', () => {
   return GlobalByDate('7/18/20')
@@ -23,4 +34,3 @@ test('GlobalByDate', () => {
 			expect(response.length).toBeGreaterThan(0)
 		})
 })
-
