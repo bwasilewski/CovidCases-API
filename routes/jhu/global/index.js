4531d6b88b4b0f004aa5488ed039caba2230b499
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-21 21:22:23 -0500

    add deaths and recovered routes

--- routes/jhu/global/index.js
+++ routes/jhu/global/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const axios = require('axios')
 const router = express.Router()
 const confirmedRouter = require('./confirmed')
+const deathsRouter = require('./deaths')
+const recoveredRouter = require('./recovered')
 
 router.use('/confirmed', confirmedRouter)
+router.use('/deaths', deathsRouter)
+router.use('/recovered', recoveredRouter)
 
 module.exports = router
