186279868fdfaf898b7c13b06828d982b7d437dd
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-21 21:21:56 -0500

    global recovered

---
+++ events/jhu/global/recovered.js
@@ -0,0 +1,38 @@
+const { getCSV } = require('../../')
+const path = require('path')
+const { filterByCountry, filterByDate } = require('../../')
+const filePath = path.join(__dirname, '../../../data/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')  
+
+
+const filterResults = (country, date, dataset) => {
+  let results = filterByCountry(country, dataset)
+	results = filterByDate(date, results)
+
+	if (results.length > 0) {
+		return [{
+			"Country/Region": results[0]["Country/Region"],
+			"date": date,
+			"total": parseInt(results[0][date])
+		}]
+	} else return []
+}
+
+module.exports = {
+  GlobalByDate: date => {
+		return new Promise((resolve, reject) => {
+			getCSV(filePath)
+				.then(results => resolve(results))
+				.catch(err => reject(err))
+		})
+	},
+	getCountry: (country, date = null) => {
+		return new Promise((resolve, reject) => {
+			getCSV(filePath)
+				.then(results => {
+					if ( date !== null ) resolve(filterResults(country, date, results))
+					else resolve(filterByCountry(country, results))
+				})	
+				.catch(err => reject(err))
+		})
+	}
+}
---
+++ routes/jhu/global/recovered.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const { 
+	getCountry,
+	CountryByDate, 
+	GlobalByDate } = require('../../../events/jhu/global/recovered')
+
+
+router.get('/', (req, res, next) => {
+  const { date } = req.query
+  GlobalByDate(date)
+		.then(response => res.json(response))
+		.catch(err => next(err))
+})
+
+
+router.get('/country/:country', (req, res, next) => {
+	const { country } = req.params
+  const { date } = req.query
+
+	getCountry(country, date)
+		.then(response => res.json(response))
+		.catch(err => next(err))
+})
+
+
+module.exports = router
