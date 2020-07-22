6ad7e995d48f752dce202dc32a75ac075ad218de
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-21 23:05:28 -0500

    stub out US confirmed cases

---
+++ events/jhu/us/confirmed.js
@@ -0,0 +1,20 @@
+const { getCSV } = require('../../')
+const path = require('path')
+const { filterByState, filterByDate } = require('../../')
+const filePath = path.join(__dirname, '../../../data/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')  
+
+
+module.exports = {
+	getUsByDate: date => {
+		return new Promise((resolve, reject) => {
+			getCSV(filePath)
+				.then(results => resolve(results))
+				.catch(err => reject(err))
+		})
+	},
+	getState: (state, date) => {
+		return new Promise((resolve, reject) => {
+
+		})
+	}
+}
