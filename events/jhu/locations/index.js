a4c5f19e36c97ee0ae8475c204041ac301f055f2
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-27 00:24:05 -0500

    a little code cleanup

--- events/jhu/locations/index.js
+++ events/jhu/locations/index.js
@@ -10,11 +10,20 @@ module.exports = {
 				.then(response => {
 					let out = []
 					if ( l.admin2 ) {
-						out = response.filter(i => i.Admin2 === l.admin2)
+						out = response.filter(i => 
+							i.Admin2 === l.admin2 && 
+							i.Province_State === l.province && 
+							i.Country_Region === l.country)
 					} else if ( l.province ) {
-						out = response.filter(i => i.Province_State === l.province && i.Admin2 === null)
+						out = response.filter(i => 
+							i.Province_State === l.province && 
+							i.Country === l.country &&
+							i.Admin2 === null)
 					} else if ( l.country ) {
-						out = response.filter(i => i.Country_Region === l.country && i.Admin2 === null && i.Province_State === null)
+						out = response.filter(i => 
+							i.Country_Region === l.country && 
+							i.Admin2 === null && 
+							i.Province_State === null)
 					}
 					resolve(out)
 				})
