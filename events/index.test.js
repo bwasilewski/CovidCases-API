ad0b1bbe7759f57d6ca40e40def21596e0da9627
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-26 22:31:54 -0500

    trying to set up a test for csv parse error

--- events/index.test.js
+++ events/index.test.js
@@ -1,14 +1,30 @@
-const { getCSV } = require('./index')
+const { getCSV, filterByCountry } = require('./index')
 const path = require('path')
 const filePath = path.join(__dirname, '../testdata/test.csv')  
+const errorFilePath = path.join(__dirname, '../testdata/testerror.csv')
 
-/*
-	filterByCountry: (country, dataset) => {
-		return dataset.filter( item => item['Country/Region'] === country )
-	},
-*/
+describe('filterByCountry', () => {
+	let testCountry = 'Albania'
+	let dataSet = [
+	 { 'Country/Region': 'Albania' },
+	 { 'Country/Region': 'Albania' },
+	 { 'Country/Region': 'Australia' },
+	 { 'Country/Region': 'US' }
+	]
 
-// describe('filterByCountry', () => {})
+	it('should return a list of objects whose `Country/Region` property matches the given country', () => {
+		expect(filterByCountry(testCountry, dataSet)).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({ 'Country/Region': 'Albania' }),
+				expect.objectContaining({ 'Country/Region': 'Albania' })
+			]))		
+
+		expect(filterByCountry('Australia', dataSet)).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({ 'Country/Region': 'Australia' })
+			]))
+	})
+})
 
 describe('getCSV', () => {
 	it('should accurately process a CSV file', () => {
@@ -23,4 +39,14 @@ describe('getCSV', () => {
 				)
 			})
 	})
+	
+  /*
+	it('should gracefully handle a parsing error', () => {
+		return getCSV(errorFilePath)
+			.then(response => console.log(response))
+			.catch(err => {
+				console.log(err)
+			})
+	})
+  */
 })
